c428664a3ede16ce9150b47ae8c727347fe4fff7
Coca-Sprite <76017078+Coca-Sprite@users.noreply.github.com>
2023-04-24 14:38:59 +0800

    Update Discord Bot儲存訊息機器人.py

--- Discord Bot儲存訊息機器人.py
+++ Discord Bot儲存訊息機器人.py
@@ -1,15 +1,13 @@
-if input("輸入要選擇哪個Bot\n1.NK4 NK4#0753\n2.Next#7616\n") == "1":
-    Token = "OTc2ODQ2MTc5MjU2MTIzMzky.Gg1tyt.aQ8K-aNJer8QXBt0GjPpwAZODtTpnxbtDsmMG8"
-else:
-    Token = "ODQ0OTU0ODg5OTIyNjc0NzM4.Gzc5DJ.U06ToI4OMYni2e_9jK2vafEgoOm1ZhTGmzQwDU"
-
+# import something
 import discord
 from discord.ext import commands
 import datetime
 import os
 
+# set variable
 client = discord.Client()
 
+# time set
 def gettime():
     x = datetime.datetime.now()
     x += datetime.timedelta(hours=0)
@@ -24,13 +22,15 @@ def gettime():
     res = f"{y}/{m}/{d} ({w}) {h}:{mi}:{s}"
     return res
 
-
+# Print bot name when link to DisocrdAPI
 @client.event
 async def on_ready():
-    print('目前登入身份：', client.user)
+    print('Bot Name：', client.user)
 
+# Main Code
 @client.event
 async def on_message(message):
+    # Print Message
     print(f"""
 NAME：{message.author}
 Message：{message.content}
@@ -38,8 +38,9 @@ guild：{message.guild}
 channel：{message.channel}
 time：{gettime()}
 ------------------------------\n""")
+    # save message in All_Message.txt
     log_file_path = os.path.join(os.getcwd(), "./Messages/All_message.txt")
-    with open(f"./Messages/All_message.txt", "a", encoding='utf8') as log:
+    with open(f"./All_Message.txt", "a", encoding='utf8') as log:
         log.write(f"""
 NAME：{message.author}
 Message：{message.content}
@@ -47,38 +48,6 @@ guild：{message.guild}
 channel：{message.channel}
 time：{gettime()}
 ------------------------------\n""")
-        
-    with open(f"./Messages/Author/{message.author}.txt", "a", encoding='utf8') as log:
-        log.write(f"""
-Message：{message.content}
-guild：{message.guild}
-channel：{message.channel}
-time：{gettime()}
-------------------------------\n""")
-    with open(f"./Messages/Content/{message.content}.txt", "a", encoding='utf8') as log:
-        log.write(f"""
-NAME：{message.author}
-guild：{message.guild}
-channel：{message.channel}
-time：{gettime()}
-------------------------------\n""")
-    with open(f"./Messages/Guild/{message.guild}.txt", "a", encoding='utf8') as log:
-        log.write(f"""
-NAME：{message.author}
-Message：{message.content}
-channel：{message.channel}
-time：{gettime()}
-------------------------------\n""")
-    with open(f"./Messages/Channel/{message.channel}.txt", "a", encoding='utf8') as log:
-        log.write(f"""
-NAME：{message.author}
-Message：{message.content}
-guild：{message.guild}
-time：{gettime()}
-------------------------------\n""")
 
-
-
-
-
-client.run(Token)
+# Replace "Your_Token" with your Code
+client.run("Your_Token")
